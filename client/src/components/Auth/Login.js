ba3e52b45ce2c8a672989e0a8024af63d209ebf9
Sumit Govil <sumg.work@gmail.com>
2019-06-21 17:46:33 +1000

    google authentication added

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    console.log("googleUser", googleUser);
+    const id_token = googleUser.getAuthResponse().id_token;
+    console.log("id_token", id_token);
+  };
+
+  const onFailure = err => {
+    console.log("Login failed", err);
+  };
+
+  return (
+    <GoogleLogin
+      clientId="73145862053-sipamua4kvh6dlrqj6hriderd86g1d9g.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
